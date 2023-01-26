f02ad843585bfe63ee351c89375707970370abee
Jorge Meza Bello <jorberiver@gmail.com>
2023-01-26 02:29:49 -0600

    update clientes listo

--- src/main/java/com/ventas/ventas/controller/ClienteController.java
+++ src/main/java/com/ventas/ventas/controller/ClienteController.java
@@ -3,9 +3,13 @@ package com.ventas.ventas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +29,16 @@ public class ClienteController {
 	public ResponseEntity<Cliente> findById(@PathVariable("id") Integer id){
 		return clienteService.findById(id)
 				.map(ResponseEntity::ok)
+				.orElseGet( () -> ResponseEntity.notFound().build());
+	}
+	@PostMapping
+	public ResponseEntity<Cliente> create(@RequestBody Cliente cliente){
+		return new ResponseEntity<>(clienteService.create(cliente), HttpStatus.CREATED);
+	}
+	@PutMapping
+	public ResponseEntity<Cliente> update(@RequestBody Cliente cliente){
+		return clienteService.findById(cliente.getIdCliente())
+				.map( cli -> ResponseEntity.ok(clienteService.update(cliente)))
 				.orElseGet( () -> ResponseEntity.notFound().build());
 	}
 }
